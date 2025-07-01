831d4059d26e430d632c8eeafb583b5a53687050
Alex3159 <alexamer59@hotmail.fr>
2025-07-01 23:03:12 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function simulateDraw() {
-    const draw = [];
-    while (draw.length < 5) {
-        let num = Math.floor(Math.random() * 49) + 1;
-        if (!draw.includes(num)) draw.push(num);
-    }
-    let chance = Math.floor(Math.random() * 10) + 1;
-    document.getElementById('result').innerText = 'Tirage : ' + draw.join(', ') + ' + Chance : ' + chance;
-}
